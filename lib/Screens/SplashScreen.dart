c8ff58e44fabf2d2206500cfe39de02fd7f5fee6
sanika2001 <porayilsanika@gmail.com>
2022-07-28 11:56:26 +0530

    Splash screen completed

--- lib/Screens/SplashScreen.dart
+++ lib/Screens/SplashScreen.dart
@@ -1,16 +1,88 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashScreen extends StatefulWidget {
   static const String id = '/splash';
-  const SplashScreen({Key? key}) : super(key: key);
+
+  SplashScreen({Key? key}) : super(key: key);
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  DateTime? currentBackPressTime;
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTime();
+  }
+
+  startTime() async {
+    var _duration = Duration(seconds: 4);
+    return Timer(_duration, () {
+      //Navigate to another screen or anyOther function, like i set duration 4 sec so this function run after 4 sec
+      //Navigator.pushReplacementNamed(context, OnboardingScreen.id);
+    });
+  }
+
+  Future<bool> onWillPop() {
+    DateTime now = DateTime.now();
+    if (currentBackPressTime == null ||
+        now.difference(currentBackPressTime!) > const Duration(seconds: 2)) {
+      currentBackPressTime = now;
+      _scaffoldKey.currentState?.showSnackBar(const SnackBar(
+          behavior: SnackBarBehavior.floating,
+          duration: Duration(seconds: 1),
+          content: Text("Press back again to exit")));
+      return Future.value(false);
+    }
+    return Future.value(true);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return WillPopScope(
+      onWillPop: onWillPop,
+      child: Scaffold(
+        key: _scaffoldKey,
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.height,
+          color: Colors.white,
+          child: AnimatedContainer(
+            duration: Duration(milliseconds: 300),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Hero(
+                  tag: "logo",
+                  child: SvgPicture.asset(
+                    "images/bibliotheca.svg",
+                    width: MediaQuery.of(context).size.width * .6739,
+                    height: MediaQuery.of(context).size.height *
+                        .279, //just like you define in pubspec.yaml file
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 18),
+                  child: Text(
+                    'Bibiliotheca',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 32,
+                      fontFamily: 'Montserrat',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
