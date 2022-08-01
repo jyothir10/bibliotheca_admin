b3fcf95a2a7bf11a4bc8bd488ddc449d886ec308
Parvathi Rajpal <parvathirajpal@gmail.com>
2022-08-01 20:55:34 +0530

    updated constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const Color black = Colors.black87;
+const Color color1 = Color(0xFF0025A9);
 const Color primaryColour = Color(0xff00B9B9);
 const TextStyle dashboardTextStyle = TextStyle(
   color: Colors.black,
